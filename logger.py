69368aafc202731b6c5cd4e845da6e09439895ae
Alex Müller <scarlettbmeza@onet.pl>
2026-05-29 09:24:15 +0100

    clean up and reorganize logger code

---
+++ logger.py
@@ -0,0 +1,30 @@
+import logging
+
+class GameLogger:
+    def __init__(self, name='GameLogger'):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+# Example usage - Uncomment below to use it
+# logger = GameLogger()
+# logger.info('Game started')
+# logger.warning('Low latency detected!')
